aaba3e79114d43cba608b395e7bded49fab88baf
Soumya <soumyadipta0077@gmail.com>
2020-10-01 11:35:49 +0530

    [Soumya] Refactor: Comparing Lines using OOPS

---
+++ LineComparision/LineComparision/Line.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LineComparision
+{
+    class Line
+    {
+        private double x1;
+        private double x2;
+        private double y1;
+        private double y2;
+        public Line(double x1, double y1, double x2, double y2)
+        {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+        }
+        public double getLength()
+        {
+            double diffX = x2 - x1;
+            double diffY = y2 - y1;
+            double lengthOfLine = Math.Sqrt(Math.Pow(diffX, 2) + Math.Pow(diffY, 2));
+            return lengthOfLine;
+        }
+        public bool equals(Line l2)
+        {
+            return (this.getLength()).Equals(l2.getLength());
+        }
+        public int comparision(Line l2)
+        {
+            return (this.getLength()).CompareTo(l2.getLength());
+        }
+    }
+}
--- LineComparision/LineComparision/Program.cs
+++ LineComparision/LineComparision/Program.cs
@@ -13,29 +13,32 @@ namespace LineComparision
             Console.WriteLine("Enter the ending point x,y co-ordinates of the 1st line");
             double x2 = Convert.ToInt32(Console.ReadLine());
             double y2 = Convert.ToInt32(Console.ReadLine());
-            double len1 = getLength(x1, y1, x2, y2);
+            Line l1 = new Line(x1,y1,x2,y2);
             Console.WriteLine("Enter the starting point x,y co-ordinates of the  2nd line");
             double x3 = Convert.ToInt32(Console.ReadLine());
             double y3 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter the ending point x,y co-ordinates of the 2nd line");
             double x4 = Convert.ToInt32(Console.ReadLine());
             double y4 = Convert.ToInt32(Console.ReadLine());
-            double len2 = getLength(x3, y3, x4, y4);
-            double difference = Math.Abs(len1 - len2);
-            int diff = len1.CompareTo(len2);
+            Line l2 = new Line(x3,y3,x4,y4);
+            Console.WriteLine("Length of line 1: "+ l1.getLength());
+            Console.WriteLine("Length of line 2: " + l2.getLength());
+            if (l1.equals(l2))
+            {
+                Console.WriteLine("Lines are of equal Length");
+            }
+            else
+            {
+                Console.WriteLine("Lines are not of equal Length");
+            }
+            double difference = l1.getLength() - l2.getLength();
+            int diff = l1.comparision(l2);
             if (diff > 0)
                 Console.WriteLine("Length of line 1 is greater than line 2 by " + difference);
             else if (diff < 0)
-                Console.WriteLine("Length of line 2 is greater than line 1 by " + difference);
+                Console.WriteLine("Length of line 2 is greater than line 1 by " + Math.Abs(difference));
             else
                 Console.WriteLine("Both lengths are equal ");
-        }
-        public static double getLength(double x1, double y1, double x2, double y2)
-        {
-            double diffX = x2 - x1;
-            double diffY = y2 - y1;
-            double lengthOfLine = Math.Sqrt(Math.Pow(diffX, 2) + Math.Pow(diffY, 2));
-            return lengthOfLine;
         }
     }
 }
